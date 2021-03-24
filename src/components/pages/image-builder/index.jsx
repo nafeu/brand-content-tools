0ef21f492e968e6f5c56f5bc6eca7b2a587ad127
Nafeu Nasir <nafeu@tenjin.com>
2021-03-23 21:03:49 -0400

    Integrate react-konva for basic image builder and export

--- src/components/pages/image-builder/index.jsx
+++ src/components/pages/image-builder/index.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import {
+  Stage,
+  Layer,
+  Rect,
+  Circle,
+  Text
+} from 'react-konva';
 
-const ImageBuilder = () => (
-  <div className="page-image-builder">
-    TODO: Add image builder page content...
-  </div>
-);
+const DEFAULT_FONT_SIZE = 20;
+const MAX_FONT_SIZE = 50;
+const MIN_FONT_SIZE = 8;
+
+const ImageBuilder = () => {
+  const [imageText, setImageText] = useState('');
+  const [fontSize, setFontSize] = useState(DEFAULT_FONT_SIZE);
+  const stage = useRef();
+
+  const downloadURI = (uri, name) => {
+    const link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const handleChangeImageText = event => {
+    setImageText(event.target.value);
+  }
+
+  const handleChangeFontSize = event => {
+    setFontSize(event.target.value);
+  }
+
+  const handleClickDownloadImage = () => {
+    const dataURL = stage.current.toDataURL({ pixelRatio: 3 });
+    downloadURI(dataURL, 'stage.png');
+  }
+
+  return (
+    <div className="page-image-builder">
+      <input value={imageText} onChange={handleChangeImageText} placeholder="Enter text"/>
+      <input
+        type="range"
+        min={MIN_FONT_SIZE}
+        max={MAX_FONT_SIZE}
+        value={fontSize}
+        onChange={handleChangeFontSize}
+      />
+      <button onClick={handleClickDownloadImage}>Download Image</button>
+      <Stage width={500} height={500} ref={stage}>
+        <Layer>
+          <Rect width={50} height={50} fill="red" />
+          <Text
+            x={10}
+            y={10}
+            text={imageText}
+            fontSize={fontSize}
+            fontFamily={'Calibri'}
+            fill={'green'}
+          />
+          <Circle x={200} y={200} stroke="black" radius={50} />
+        </Layer>
+      </Stage>
+    </div>
+  )
+};
 
 export default ImageBuilder;
