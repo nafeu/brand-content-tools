0aaf4fd13841216281103b95535811d8e625ed4d
Nafeu Nasir <nafeu@tenjin.com>
2021-03-24 11:18:04 -0400

    Add test images and basic builder logic

--- src/components/pages/image-builder/index.jsx
+++ src/components/pages/image-builder/index.jsx
@@ -4,16 +4,43 @@ import {
   Layer,
   Rect,
   Circle,
-  Text
+  Text,
+  Image
 } from 'react-konva';
+import useImage from 'use-image';
 
 const DEFAULT_FONT_SIZE = 20;
 const MAX_FONT_SIZE = 50;
 const MIN_FONT_SIZE = 8;
 
+const DEFAULT_BACKGROUND_POSITION = {
+  isDragging: false,
+  x: 0,
+  y: 0
+}
+
+const DEFAULT_LOGO_POSITION = {
+  isDragging: false,
+  x: 0,
+  y: 0
+}
+
+const BackgroundImage = imageProps => {
+  const [image] = useImage('/assets/bg-1.jpg');
+  return <Image image={image} {...imageProps} />;
+}
+
+const LogoImage = imageProps => {
+  const [image] = useImage('/assets/logo-2.png');
+  return <Image image={image} {...imageProps} />;
+}
+
 const ImageBuilder = () => {
   const [imageText, setImageText] = useState('');
   const [fontSize, setFontSize] = useState(DEFAULT_FONT_SIZE);
+  const [backgroundPosition, setBackgroundPosition] = useState(DEFAULT_BACKGROUND_POSITION);
+  const [logoPosition, setLogoPosition] = useState(DEFAULT_LOGO_POSITION);
+
   const stage = useRef();
 
   const downloadURI = (uri, name) => {
@@ -30,12 +57,42 @@ const ImageBuilder = () => {
   }
 
   const handleChangeFontSize = event => {
-    setFontSize(event.target.value);
+    setFontSize(Number(event.target.value));
   }
 
   const handleClickDownloadImage = () => {
     const dataURL = stage.current.toDataURL({ pixelRatio: 3 });
     downloadURI(dataURL, 'stage.png');
+  }
+
+  const handleDragStartLogo = () => {
+    setLogoPosition({
+      ...logoPosition,
+      isDragging: true
+    })
+  }
+
+  const handleDragEndLogo = event => {
+    setLogoPosition({
+      isDragging: false,
+      x: event.target.x(),
+      y: event.target.y()
+    })
+  }
+
+  const handleDragStartBackground = () => {
+    setBackgroundPosition({
+      ...logoPosition,
+      isDragging: true
+    })
+  }
+
+  const handleDragEndBackground = event => {
+    setBackgroundPosition({
+      isDragging: false,
+      x: event.target.x(),
+      y: event.target.y()
+    })
   }
 
   return (
@@ -51,16 +108,28 @@ const ImageBuilder = () => {
       <button onClick={handleClickDownloadImage}>Download Image</button>
       <Stage width={500} height={500} ref={stage}>
         <Layer>
-          <Rect width={50} height={50} fill="red" />
+          <BackgroundImage
+            draggable
+            x={backgroundPosition.x}
+            y={backgroundPosition.y}
+            onDragStart={handleDragStartBackground}
+            onDragEnd={handleDragEndBackground}
+          />
+          <LogoImage
+            draggable
+            x={logoPosition.x}
+            y={logoPosition.y}
+            onDragStart={handleDragStartLogo}
+            onDragEnd={handleDragEndLogo}
+          />
           <Text
             x={10}
             y={10}
             text={imageText}
             fontSize={fontSize}
-            fontFamily={'Calibri'}
-            fill={'green'}
+            fontFamily={'Fira Code'}
+            fill={'white'}
           />
-          <Circle x={200} y={200} stroke="black" radius={50} />
         </Layer>
       </Stage>
     </div>
